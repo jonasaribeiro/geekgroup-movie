17c60be06b10bb4845cfedf683f4200059be450b
Jonas <jonasafribeiro@gmail.com>
2023-03-06 14:40:42 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { GlobalStyles } from './styles/global';
+import Router from './routes';
+
+function App() {
+    return (
+        <>
+            <GlobalStyles />
+            <Router />
+        </>
+    );
+}
+
+export default App;
---
+++ src/routes.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+
+const Router = () => (
+    <Routes>
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+    </Routes>
+);
+
+export default Router;
