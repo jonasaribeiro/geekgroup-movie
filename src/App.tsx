59e8fccff4fdcfe2ef2e9bdb6c6beda72980c309
gabriel <gabriel.c8944@gmail.com>
2023-03-07 16:11:25 -0300

    feature:push33

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { GlobalStyles } from './styles/global';
 import Router from './routes';
 import { UserProvider } from './provider/UserContext';
-import { UserProvider } from './provider/UserContext';
-import { MoviePage } from './pages/MoviePage/moviepage';
 
 function App() {
     return (
