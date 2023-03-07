30d6e83f9050d807ccf5e31f6eb93264b6fe233a
Jonas <jonasafribeiro@gmail.com>
2023-03-07 15:20:48 -0300

    Fix: Typagem do children do Header adicionado

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,8 @@
+import { ReactNode } from 'react';
 import logo from '../../assets/img/logo.svg';
 import { Img, StyledHeader } from './header.styled';
 
-export const Header = ({ children }) => {
+export const Header = ({ children }: { children: ReactNode }) => {
     return (
         <StyledHeader>
             <figure>
