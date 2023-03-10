bbcee77a59d96635c139932e94461213df4690f9
gabriel <gabriel.c8944@gmail.com>
2023-03-10 15:27:31 -0300

    feat:description page com funcionalidades de comentar e ver comentários

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,13 +3,15 @@ import { ProfilePage } from './pages/Profile/profile';
 import { ProtectedPages } from './pages/.ProtectedPages/ProtectedPages';
 import { LogoffPages } from './pages/.LogoffPages/LodoffPages';
 import { RegisterPage } from './pages/RegisterPage/registerPage';
+import { MoviePage } from './pages/MoviePage/moviepage';
 
 const Router = () => (
     <Routes>
         <Route path='/' />
         <Route path='*' />
+        <Route path='/movieinfo/:id' element={<MoviePage />} />
         <Route path='/' element={<ProtectedPages />}>
-            <Route path='/movieinfo/:id' />
+            {/* <Route path='/movieinfo/:id' /> */}
             <Route path='/profile' element={<ProfilePage />} />
         </Route>
         <Route path='/' element={<LogoffPages />}>
