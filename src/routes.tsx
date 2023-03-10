166f8d6a0a90370993aa42e6dbcaf81aec30f54e
gabriel <gabriel.c8944@gmail.com>
2023-03-10 15:41:59 -0300

    feature:faltando apenas logica dos comentários e detalhes do header

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,13 +4,15 @@ import { ProtectedPages } from './pages/.ProtectedPages/ProtectedPages';
 import { LogoffPages } from './pages/.LogoffPages/LodoffPages';
 import { RegisterPage } from './pages/RegisterPage/registerPage';
 import { NotFoundPage } from './pages/NotFoundPage/err404';
+import { MoviePage } from './pages/MoviePage/moviepage';
 
 const Router = () => (
     <Routes>
         <Route path='/' />
         <Route path='*' element={<NotFoundPage />} />
+        <Route path='/movieinfo/:id' element={<MoviePage/>} />
         <Route path='/' element={<ProtectedPages />}>
-            {/* <Route path='/movieinfo/:id' /> */}
+            {/* <Route path='/movieinfo/:id' element={<MoviePage/>} /> */}
             <Route path='/profile' element={<ProfilePage />} />
         </Route>
         <Route path='/' element={<LogoffPages />}>
