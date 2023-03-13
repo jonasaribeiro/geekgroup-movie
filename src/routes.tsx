be2b36e26bcbe84eabc7c1d427f3cba1926f1984
Jonas <jonasafribeiro@gmail.com>
2023-03-13 17:52:38 -0300

    Fix: Configurada rotas e localStorage corretamente

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,10 +5,13 @@ import { LogoffPages } from './pages/.LogoffPages/LodoffPages';
 import { RegisterPage } from './pages/RegisterPage/registerPage';
 import { NotFoundPage } from './pages/NotFoundPage/err404';
 import { MoviePage } from './pages/MoviePage/moviepage';
+import { LandingPage } from './pages/LandingPage/landingpage';
+import { LoginPage } from './pages/LoginPage/LoginPage';
 
 const Router = () => (
     <Routes>
-        <Route path='/' />
+        <Route path='/' element={<LoginPage />} />
+        <Route path='/info' element={<LandingPage />} />
         <Route path='*' element={<NotFoundPage />} />
         <Route path='/' element={<ProtectedPages />}>
             <Route path='/movieinfo/:id' element={<MoviePage />} />
